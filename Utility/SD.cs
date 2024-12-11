75e31029fc2d869fb1677f8b82bd96a9086e27d0
Loredana Siretchi <siretchi.loredana@gmail.com>
2024-12-11 13:41:16 +0000

    Assigning Roles to Users - Redesign Register & Login page and Create new NavBar for Customer

---
+++ Utility/SD.cs
@@ -0,0 +1,8 @@
+﻿namespace PresizelyWeb.Utility
+{
+    public static class SD
+    {
+        public static string Role_Admin = "Admin";
+        public static string Role_Customer = "Customer";
+    }
+}
